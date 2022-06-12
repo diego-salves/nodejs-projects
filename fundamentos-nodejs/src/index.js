833b226c3a795409f08216bf1247d11c6ba733c5
diego-salves <diegosantos57@outlook.com>
2022-06-12 18:04:37 -0300

    Created all paths of the API and Insomnia integrated

--- fundamentos-nodejs/src/index.js
+++ fundamentos-nodejs/src/index.js
@@ -8,6 +8,18 @@ app.get("/courses", (req, res) => {
 
 app.post("/courses", (req, res) => {
     return res.json(["Curso 1", "Curso 2", "Curso 3", "Curso 4"]);
-})
+});
+
+app.put("/courses/:id", (req, res) => {
+    return res.json(["Curso 6", "Curso 2", "Curso 3", "Curso 4"]);
+});
+
+app.patch("/courses/:id", (req, res) => {
+    return res.json(["Curso 6", "Curso 7", "Curso 3", "Curso 4"]);
+});
+
+app.delete("/courses/:id", (req, res) => {
+    return res.json(["Curso 6", "Curso 2", "Curso 4"]);
+});
 
 app.listen(3333);
