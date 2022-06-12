06c3ac1dbe3b2175ab9baf51489d1d57823fc0e7
diego-salves <diegosantos57@outlook.com>
2022-06-12 02:42:20 -0300

    adicionando nodemon para configuração de ambiente dev

--- fundamentos-nodejs/src/index.js
+++ fundamentos-nodejs/src/index.js
@@ -3,8 +3,9 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    return res.json({ message: "Hello World!"});
+    return res.json({ message: "Nodemon test"});
 });
 
 //port where the aplication runs
 app.listen(3333);
+
