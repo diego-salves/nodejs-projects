34b03ac8d9d6e4dc143ef9906f8835a2b60441b8
diego-salves <diegosantos57@outlook.com>
2022-06-14 00:39:11 -0300

    updated route, query and body params

--- fundamentos-nodejs/src/index.js
+++ fundamentos-nodejs/src/index.js
@@ -3,14 +3,20 @@ const express = require("express");
 const app = express();
 
 app.get("/courses", (req, res) => {
+    const query = request.query;
+    console.log(query);
     return res.json(["Curso 1", "Curso 2", "Curso 3"])
 });
 
 app.post("/courses", (req, res) => {
+    const body = request.body;
+    console.log(body);
     return res.json(["Curso 1", "Curso 2", "Curso 3", "Curso 4"]);
 });
 
 app.put("/courses/:id", (req, res) => {
+    const { id } = request.params;
+    console.log(params);
     return res.json(["Curso 6", "Curso 2", "Curso 3", "Curso 4"]);
 });
 
@@ -23,3 +29,11 @@ app.delete("/courses/:id", (req, res) => {
 });
 
 app.listen(3333);
+
+/**
+ * Tipos de parametros: 
+ * 
+ * Route Params => Identificar um recurso, editar/deletar/buscar recurso
+ * Query Params => Paginação, filtros, ordenação
+ * Body Params => Dados para criação/edição de um recurso
+ */
